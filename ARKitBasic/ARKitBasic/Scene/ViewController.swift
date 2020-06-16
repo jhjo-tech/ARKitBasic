fc3388e2d02defcc8af8e10c41b5c8f33b172639
jhjo <jhjo.tech@gmail.com>
2020-06-16 21:45:12 +0900

    init.

---
+++ ARKitBasic/ARKitBasic/Scene/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARKitBasic
+//
+//  Created by Jo JANGHUI on 2020/06/16.
+//  Copyright © 2020 Jo JANGHUI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
